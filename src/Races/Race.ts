93c28c9f9f123bbcad0399bce51a88c4b844c58f
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-15 00:55:38 -0300

    feat: create class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+export default abstract class Race {
+  private static _createdRacesInstances = 0;
+
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static get createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
