42005c45db4c034ca28dc31f8793b7caa11bacd8
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-15 02:58:39 -0300

    feat(req04): create class Archtype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,34 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  static _createdArchetypeInstances = 0;
+
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+    Archetype._createdArchetypeInstances += 1;
+  }
+
+  static createdArchtypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+}
